5a4ae10ba810f209f6a8fdc43c02b62955372d79
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-09-10 21:52:37 -0700

    move jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  coverageReporters: [
+    "json"
+  ],
+  transform: {
+    "^.+\\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+  },
+  testRegex: "\\.test\\.ts$",
+  moduleFileExtensions: [
+    "js",
+    "ts"
+  ]
+}
