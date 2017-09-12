6f397b98a34255f46381abf0125b40e2cfeb93c7
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-09-11 18:22:34 -0700

    export IFlag

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { IArg } from './args'
 export { parse, ParserInput, ParserOutput, InputArgs, InputFlags } from './parse'
-export { flags } from './flags'
+export { flags, IFlag } from './flags'
