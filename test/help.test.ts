bbcf2081c53d4c051c93ad5920a2bd336db886e7
Sam Harrison <sharrison@twilio.com>
2019-04-22 15:26:30 -0500

    Add boolean flag usage test

--- test/help.test.ts
+++ test/help.test.ts
@@ -14,11 +14,13 @@ describe('flagUsage', () => {
       flags.string({name: 'bar', char: 'b', description: 'bar'}),
       flags.string({name: 'foo', char: 'f', description: 'desc'}),
       flags.string({name: 'foo', char: 'f', helpLabel: '-f'}),
+      flags.boolean({char: 'g', description: 'goo'}),
     ]
     expect(flagUsages(f)).to.deep.equal([
       [' -b, --bar BAR', 'bar'],
       [' -f, --foo FOO', 'desc'],
       [' -f FOO', undefined],
+      [' -g', 'goo'],
       [' --bak BAK', undefined],
       [' --baz BAZ', 'baz'],
     ])
